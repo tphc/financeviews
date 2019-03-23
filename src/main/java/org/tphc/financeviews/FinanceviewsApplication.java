4b025caa89ad92127f2789a9bc8418dd73131bed
tphc <tphc0@outlook.com>
2019-03-23 21:49:31 +0100

    cleanup

--- src/main/java/org/tphc/financeviews/FinanceviewsApplication.java
+++ src/main/java/org/tphc/financeviews/FinanceviewsApplication.java
@@ -3,7 +3,10 @@ package org.tphc.financeviews;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -11,9 +14,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.data.repository.PagingAndSortingRepository;
-
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.util.ResourceUtils;
@@ -21,26 +22,41 @@ import org.springframework.util.ResourceUtils;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RepositoryRestResource(collectionResourceRel = "stockTs", path = "data")
+interface StockTsRepository extends PagingAndSortingRepository<StockTs, Long> {
+    List<StockTs> findAllBy();
+
+    List<StockTs> findByStockName(@Param("name") String name);
+
+    List<StockTs> findByStockTicker(@Param("ticker") String ticker);
+}
+
+@RepositoryRestResource(collectionResourceRel = "stock", path = "stock")
+interface StockRepository extends PagingAndSortingRepository<Stock, Long> {
+    List<Stock> findByName(@Param("name") String name);
+
+    List<Stock> findByTicker(@Param("ticker") String ticker);
+}
 
 @SpringBootApplication
 public class FinanceviewsApplication {
 
     private static final Logger log = LoggerFactory.getLogger(FinanceviewsApplication.class);
+    private final StockTsRepository stockTsRepository;
+    private final StockRepository stockRepository;
 
     public FinanceviewsApplication(StockTsRepository stockTsRepository, StockRepository stockRepository) {
         this.stockTsRepository = stockTsRepository;
         this.stockRepository = stockRepository;
     }
 
-
     public static void main(String[] args) {
         SpringApplication.run(FinanceviewsApplication.class, args);
     }
-
-    private final StockTsRepository stockTsRepository;
-
-    private final StockRepository stockRepository;
 
     private String uid() {
         return UUID.randomUUID().toString();
@@ -113,19 +129,6 @@ class CompDump {
     private String actSymbol;
     @JsonProperty(value = "Company Name")
     private String companyName;
-}
-
-@RepositoryRestResource(collectionResourceRel = "stockTs", path = "data")
-interface StockTsRepository extends PagingAndSortingRepository<StockTs, Long> {
-    List<StockTs> findAllBy();
-    List<StockTs> findByStockName(@Param("name") String name);
-    List<StockTs> findByStockTicker(@Param("ticker") String ticker);
-}
-
-@RepositoryRestResource(collectionResourceRel = "stock", path = "stock")
-interface StockRepository extends PagingAndSortingRepository<Stock, Long> {
-    List<Stock> findByName(@Param("name") String name);
-    List<Stock> findByTicker(@Param("ticker") String ticker);
 }
 
 @Entity
